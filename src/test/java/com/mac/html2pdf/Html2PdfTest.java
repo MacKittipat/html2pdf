a951b9cb1547b265287693d071f932ffef57e1fb
Mac <mac@abctech-thailand.com>
2013-10-24 11:09:36 +0700

    Fix pdfPath in unit test

--- src/test/java/com/mac/html2pdf/Html2PdfTest.java
+++ src/test/java/com/mac/html2pdf/Html2PdfTest.java
@@ -7,6 +7,7 @@ public class Html2PdfTest {
     @Test
     public void testGeneratePdf() throws Exception {
         Html2Pdf html2Pdf = new Html2Pdf();
-        html2Pdf.generatePdf("<html><head><title>test</title></head><body>test</body></html>", "/home/mac/test.pdf");
+        String pdfPath = this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath() + "test.pdf";
+        html2Pdf.generatePdf("<html><head><title>test</title></head><body>test</body></html>", pdfPath);
     }
 }
