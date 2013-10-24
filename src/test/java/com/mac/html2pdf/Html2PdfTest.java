ae7998ac470a3b0fc2230e9d542f52b3017d7096
Mac <mac@abctech-thailand.com>
2013-10-24 11:00:49 +0700

    Create unit test

---
+++ src/test/java/com/mac/html2pdf/Html2PdfTest.java
@@ -0,0 +1,11 @@
+package com.mac.html2pdf;
+
+import org.junit.Test;
+
+public class Html2PdfTest {
+
+    @Test
+    public void testGeneratePdf() throws Exception {
+        Html2Pdf html2Pdf = new Html2Pdf();
+    }
+}
